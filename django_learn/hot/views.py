41452215d22d183d357c533756485c9b0d74e7da
Turishchev <168584108+Turishchev@users.noreply.github.com>
2025-01-04 18:00:31 +0500

    шаблоны

--- django_learn/hot/views.py
+++ django_learn/hot/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+
+
+def index(request):
+    header =  "User's data"
+    langs = ['Python', 'CSS', 'JS']
+    user = {'name': 'Nikita', 'age': 26}
+    address = ('Фронтовиков', '8/3', 633)
+
+    data = {'header': header, 'langs': langs, 'user': user, 'address': address}
+    return render(request, 'hot/index.html', context=data)

7c034dd0f5cc02215bb60d50a724629bbf619caf
Turishchev <168584108+Turishchev@users.noreply.github.com>
2025-01-12 14:44:53 +0500

    Teach models

--- django_learn/hot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from hot import views
-
-
-urlpatterns = [
-    path('',views.index),
-    path('about/', views.about)
-]
--- django_learn/hot/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'base.html')
-
-
-def about(request):
-    return render(request, 'hot/about.html')

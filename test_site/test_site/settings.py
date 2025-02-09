8824fc2fbf9381c12ed4f2e99a52ad587998e595
Turishchev <168584108+Turishchev@users.noreply.github.com>
2025-02-09 22:41:10 +0300

    update login and logout

--- test_site/test_site/settings.py
+++ test_site/test_site/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': '',
+        'PASSWORD': 'n9i8ki3t0a2020',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -156,3 +156,5 @@ EMAIL_ADMIN = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
